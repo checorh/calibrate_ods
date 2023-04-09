5ca874a874859ed08fbcd0add5863b7938282ee2
Sergio Rodriguez <rodrse@amazon.com>
2023-04-09 15:52:36 -0700

    Updated helper functions

--- sumo_python/helpers/generate_od_xml.py
+++ sumo_python/helpers/generate_od_xml.py
@@ -1,38 +1,21 @@
 import numpy as np
 
-def generate_od_xml(od_rand, config, sim_setup, prefix_run):
-    """
-    The tazRelation format defines the demand per OD pair in time slices for 
-    every a given vehicle type as follows:
+import xml.etree.ElementTree as ET
+from pathlib import Path
 
-        <data xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://sumo.dlr.de/xsd/datamode_file.xsd">
-            <interval id="car" begin="0" end="1:0:0">
-            <tazRelation count="2000" from="1" to="2"/>
-            <tazRelation count="500" from="1" to="3"/>
-            ...
-            </interval>
-            <interval ...>
-            ...
-        </data>
 
-    See: https://sumo.dlr.de/docs/Demand/Importing_O/D_Matrices.html#tazrelation_format
-    """
-    # print od in xml file
+def generate_od_xml(x, config, sim_setup, prefix_run):
 
-    od_xml_str = (
-        f"<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n"
-        f"<data xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:noNamespaceSchemaLocation=\"http://sumo.dlr.de/xsd/datamode_file.xsd\">\n"
-        f"<interval id=\"CarA\" begin=\"54000\" end=\"57600.0\">\n"
-        f"<tazRelation from=\"taz91\" to=\"taz93\" count=\"{int(np.floor(od_rand[0]))}\"/>\n"
-        f"<tazRelation from=\"taz91\" to=\"taz94\" count=\"{int(np.floor(od_rand[1]))}\"/>\n"
-        f"<tazRelation from=\"taz92\" to=\"taz93\" count=\"{int(np.floor(od_rand[2]))}\"/>\n"
-        f"<tazRelation from=\"taz92\" to=\"taz94\" count=\"{int(np.floor(od_rand[3]))}\"/>\n"
-        f"</interval>\n"
-        f"</data>"
-    )
+    # check if initial 
+    init_od_path = f"{config['NETWORK']}/{sim_setup['init_od']}"
+    if Path(init_od_path).is_file():
+        print("Reading:",init_od_path)
+        tree = ET.parse(init_od_path)
+        root = tree.getroot()
 
+        for i,x in enumerate(x):
+            root[0][i].attrib["count"] = str(np.round(x))
+            
     file_name = f"{config['NETWORK']}/{prefix_run}_{sim_setup['current_od']}"
-    print('printing '+file_name)
-    f = open(file_name,'w')
-    f.write(od_xml_str) 
-    f.close()
+    print('Saving: '+file_name)
+    tree.write(file_name)
--- sumo_python/helpers/load_experiment_metadata.py
+++ sumo_python/helpers/load_experiment_metadata.py
@@ -1,35 +1,12 @@
 import json
 from pathlib import Path
-from typing import Union
 
-def load_experiment_config(
-                        config: Union[Path, str]="config.json",
-                        sim_setup: Union[Path, str]="simulation_setups.json"
-    ):
-    """Load paths, simulation setups and algorithm setups
+def load_experiment_metadata(input_path: str):
+
+    config = json.load(open(input_path + "/config.json"))
+    config["NETWORK"] = Path(config["NETWORK"])
+    config["SUMO"] = Path(config["SUMO"])
     
+    sim_setup = json.load(open(input_path + "simulation_setups.json"))
 
-    Parameters
-    ----------
-    config : Union[Path, str], optional
-        Paths to cache, network, and SUMO. The default is "config.json".
-    sim_setup : Union[Path, str], optional
-        Simulation parameters. The default is "simulation_setups.json".
-
-    Returns
-    -------
-    config : Dictionary
-    sim_setup : Dictionary
-
-    """
-    config = Path(config) if isinstance(config, str) else config
-    config = json.load(open(config))
-    for k, v in config.items():
-        config[k] = Path(v)
-    
-    sim_setup = Path(sim_setup) if isinstance(sim_setup, str) else sim_setup
-    sim_setup = json.load(open(sim_setup))
-    for k, v in sim_setup.items():
-        sim_setup[k] = v
-            
     return config, sim_setup
